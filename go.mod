67e7cfac02e4c083f85637deff37f9ff3aa4ae84
david <davidstyrbjorn@gmail.com>
2024-08-05 23:09:45 +0200

    feat: raygui + text input + isbn lookup all hooked up!
    not 100% sure we will actually use raylib
    one more thing to test before is the capability to make an interactive list
    i dont want it to be stupid hard to achieve a table-list behaviour when
    listing all the books in the database! backup framework is Fyne!

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module klaras-book-vault
 
 go 1.22.0
 
-require github.com/mattn/go-sqlite3 v1.14.22 // indirect
+require (
+	github.com/ebitengine/purego v0.7.1 // indirect
+	github.com/gen2brain/raylib-go/raygui v0.0.0-20240628125141-62016ee92fc0 // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20240628125141-62016ee92fc0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -8,6 +9,8 @@ import (
 	"database/sql"
 	"net/http"
 
+	gui "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -26,7 +29,11 @@ type Book struct {
 	status      string
 }
 
-func LookupBookFromISBN(isbn string) Book {
+type ISBNResponse struct {
+	title string
+}
+
+func LookupBookFromISBN(isbn string) ISBNResponse {
 	// http://openlibrary.org/api/books?bibkeys=ISBN:1931498717&jscmd=details&format=json
 
 	//TODO:  Find out the info we need to fill out a book struct
@@ -40,25 +47,35 @@ func LookupBookFromISBN(isbn string) Book {
 		log.Fatal(err)
 	}
 
-	sb := string(body)
-	log.Printf(sb)
+	stringResult := string(body)
 
-	return Book{
-		isbn:        "",
-		title:       "",
-		currentPage: 0,
-		status:      UNREAD,
+	var data map[string]interface{}
+	err = json.Unmarshal([]byte(stringResult), &data)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	var isbnResponse ISBNResponse
+	for _, v := range data {
+		details := v.(map[string]interface{})["details"].(map[string]interface{})
+		isbnResponse.title = details["title"].(string)
+	}
+
+	return isbnResponse
 }
 
 func FuzzySearchBooks(books []Book, search string) []Book {
 	return books
 }
 
-func main() {
+func main2() {
 	fmt.Println("Library Init")
 
-	LookupBookFromISBN("9780060598242")
+	isbnResponse := LookupBookFromISBN("9780060598242")
+	log.Printf(isbnResponse.title)
+
+	isbnResponse = LookupBookFromISBN("9780553213690")
+	log.Printf(isbnResponse.title)
 
 	return
 
@@ -93,4 +110,78 @@ func main() {
 	}
 
 	fmt.Println("Inserted a book 'edde' into the Books table successfully!")
+}
+
+const Width = 1280
+const Height = 720
+
+func main() {
+	rl.InitWindow(Width, Height, "Klaras Bok Valv")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	// var button bool
+
+	fontTtf := rl.LoadFont("fonts/Meditative.ttf")
+	welcomeMsg := "Klaras Bok Valv"
+	msgPosition := rl.Vector2{}
+	scale := float32(fontTtf.BaseSize)
+	msgPosition.X = float32(Width)/2 - rl.MeasureTextEx(fontTtf, welcomeMsg, scale, 0).X/2
+	msgPosition.Y = float32(Height)/2 - rl.MeasureTextEx(fontTtf, welcomeMsg, scale, 0).X/2
+
+	textInput := ""
+
+	var button bool
+	buttonWidth := float32(120)
+	buttonHeight := float32(60)
+	buttonX := float32(Width)/2 - buttonWidth/2
+	buttonY := float32(Height)/2 - buttonHeight/2 + 120
+
+	var isbnResponse ISBNResponse
+	isbnPosition := rl.Vector2{}
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.Beige)
+
+		key := rl.GetCharPressed()
+		for {
+			if key <= 0 {
+				break
+			}
+
+			if key > 0 {
+				if key >= 32 && key <= 125 {
+					textInput += string(rune(key))
+				}
+			}
+
+			key = rl.GetCharPressed()
+		}
+
+		if rl.IsKeyPressed(rl.KeyBackspace) && len(textInput) != 0 {
+			textInput = textInput[0 : len(textInput)-1]
+		}
+
+		button = gui.Button(rl.NewRectangle(buttonX, buttonY, buttonWidth, buttonHeight), "Search")
+		if button {
+			go func() {
+				isbnResponse = LookupBookFromISBN(textInput)
+				isbnPosition.X = float32(Width)/2 - rl.MeasureTextEx(fontTtf, isbnResponse.title, scale, 0).X/2
+				isbnPosition.Y = msgPosition.Y + 130
+			}()
+		}
+
+		rl.DrawTextEx(fontTtf, "Klaras Bok Valv", msgPosition, scale, 0, rl.Black)
+		rl.DrawTextEx(fontTtf, isbnResponse.title, isbnPosition, scale, 0, rl.White)
+
+		inputPosition := rl.Vector2{}
+		inputPosition.X = float32(Width)/2 - rl.MeasureTextEx(fontTtf, textInput, scale, 0).X/2
+		inputPosition.Y = float32(Height)/2 - rl.MeasureTextEx(fontTtf, textInput, scale, 0).Y/2
+		rl.DrawTextEx(fontTtf, textInput, inputPosition, scale, 0, rl.White)
+
+		rl.EndDrawing()
+	}
 }
